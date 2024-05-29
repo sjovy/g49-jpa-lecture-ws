f7123b08926f919ed962b2462133acd6eaa28b1c
sjovy <159021500+sjovy@users.noreply.github.com>
2024-05-29 10:49:45 +0200

    Initial commit med part1 klart

---
+++ src/main/java/se/lexicon/g49jpalecturews/G49JpaLectureWsApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49jpalecturews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G49JpaLectureWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G49JpaLectureWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/g49jpalecturews/G49JpaLectureWsApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g49jpalecturews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G49JpaLectureWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
