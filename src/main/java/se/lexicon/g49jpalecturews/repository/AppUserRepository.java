7a6b9576abb2ad20e8c2b59d80c0e95322a822ad
sjovy <159021500+sjovy@users.noreply.github.com>
2024-05-29 12:57:20 +0200

    Initial commit med part1 och part2 klart

--- src/main/java/se/lexicon/g49jpalecturews/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/g49jpalecturews/repository/AppUserRepository.java
@@ -1,6 +1,5 @@
 package se.lexicon.g49jpalecturews.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.lexicon.g49jpalecturews.entity.AppUser;
